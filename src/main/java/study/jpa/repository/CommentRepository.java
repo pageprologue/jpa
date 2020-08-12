dc8b6ca9b8e6b9d1ac05abad1bc0c1190dcdd7e3
Chloe <page.prologue@gmail.com>
2020-08-12 14:23:52 +0900

    Query Example - PageRequest, Stream

--- src/main/java/study/jpa/repository/CommentRepository.java
+++ src/main/java/study/jpa/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package study.jpa.repository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.RepositoryDefinition;
 
 import study.jpa.domain.Comment;
@@ -9,8 +12,13 @@ import study.jpa.domain.Comment;
 @RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
 public interface CommentRepository extends CommonRepository<Comment, Long> {
 
+    
     List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount);
-
+    
     List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountDesc(String keyword);
     
+    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable page);
+
+    Stream<Comment> findByCommentContainsIgnoreCaseOrderByIdAsc(String keyword);
+
 }
--- src/test/java/study/jpa/CommentRepositoryTests.java
+++ src/test/java/study/jpa/CommentRepositoryTests.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import study.jpa.domain.Comment;
 import study.jpa.repository.CommentRepository;
@@ -62,6 +66,20 @@ public class CommentRepositoryTests {
         List<Comment> orderByComments = commentRepository.findByCommentContainsIgnoreCaseOrderByLikeCountDesc("jpa");
         // Then
         assertThat(orderByComments).first().hasFieldOrPropertyWithValue("likeCount", 100);
+
+        /* PageRequest Page List */
+        // When
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.ASC, "likeCount"));
+        Page<Comment> pageComments = commentRepository.findByCommentContainsIgnoreCase("jpa", pageRequest);
+        // Then
+        assertThat(pageComments.getTotalElements()).isEqualTo(4);
+        assertThat(pageComments).first().hasFieldOrPropertyWithValue("likeCount", 1);
+
+        /* PageRequest Page List */
+        try(Stream<Comment> streamComments = commentRepository.findByCommentContainsIgnoreCaseOrderByIdAsc("jpa")) {
+            Comment firstComment = streamComments.findFirst().get();
+            assertThat(firstComment.getLikeCount()).isEqualTo(1);
+        }
     }
 
     private void createComment(String keyword, int likeCount) {
