95280f376a5ebeaa54683935bfc3d5a13f6800cc
Chloe <page.prologue@gmail.com>
2020-08-17 23:41:12 +0900

    change eventListener

--- src/main/java/study/jpa/config/PostRepositoryConfig.java
+++ src/main/java/study/jpa/config/PostRepositoryConfig.java
@@ -1,16 +1,20 @@
 package study.jpa.config;
 
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import study.jpa.listener.PostListener;
+import study.jpa.domain.PostPublishedEvent;
 
 @Configuration
 public class PostRepositoryConfig {
     
     @Bean
-    public PostListener postListener() {
-        return new PostListener();
+    public ApplicationListener<PostPublishedEvent> postListener() {
+        return event -> {
+                System.out.println("============================");
+                System.out.println(event.getPost().getTitle() + "is published!");
+                System.out.println("============================");
+        };
     }
-    
 }
